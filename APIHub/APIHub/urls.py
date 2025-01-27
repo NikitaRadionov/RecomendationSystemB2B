75eb524cc81a75ed1d622ad1114e70ff0e468aa8
Nikita <72099127+NikitaRadionov@users.noreply.github.com>
2025-01-28 00:04:39 +0300

    Update urls.py

--- APIHub/APIHub/urls.py
+++ APIHub/APIHub/urls.py
@@ -16,8 +16,26 @@ Including another URLconf
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="Документация к API",
+      terms_of_service="",
+    #   contact=openapi.Contact(email="contact@myapi.com"),
+    #   license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
