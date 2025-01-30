dabf4421d3d6a2baeb0c5b38544d8d77a172bae0
Nikita <72099127+NikitaRadionov@users.noreply.github.com>
2025-01-30 20:25:31 +0300

    Update apps.py

--- APIHub/api/apps.py
+++ APIHub/api/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+from .ml_model import MLModel
+import os
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        base_dir = os.path.join(os.path.dirname(__file__), "model_files")
+
+        model_path = os.path.join(base_dir, "rec_model")
+        region_encoder_path = os.path.join(base_dir, "region_encoder.json")
+        supplier_encoder_path = os.path.join(base_dir, "supplier_encoder.json")
+        description_model_path = os.path.join(base_dir, "description_model.sav")
+
+        self.ml_model = MLModel(model_path, region_encoder_path, supplier_encoder_path, description_model_path)
